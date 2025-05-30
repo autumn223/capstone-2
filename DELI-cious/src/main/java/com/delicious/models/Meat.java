15d5a86391dc25b6f77bcb2fefaf8d04e856b8b0
autumn_Middlebrooks_Willis <autumnm3841@gmail.com>
2025-05-30 05:16:57 -0400

    complete

--- DELI-cious/src/main/java/com/delicious/models/Meat.java
+++ DELI-cious/src/main/java/com/delicious/models/Meat.java
@@ -1,9 +1,5 @@
 package com.delicious.models;
 
-/**
- * Represents a meat topping on a sandwich.
- * Meats are free by default, but extra meat costs $1.00.
- */
 public class Meat extends Topping {
     public Meat(String name, boolean isExtra) {
         super(name, isExtra);
